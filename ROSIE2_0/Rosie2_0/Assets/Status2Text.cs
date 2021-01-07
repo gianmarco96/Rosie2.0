32e6530054735972c7176feaf6938e18ce5aaec1
gianmarco96 <gianmarco.pisanelli@gmail.com>
2021-01-07 14:05:30 +0000

    Adding Unity environment

---
+++ ROSIE2_0/Rosie2_0/Assets/Status2Text.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Status2Text : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject Texttest = new GameObject("Texttest", typeof(RectTransform));
+        Texttest.transform.SetParent(this.transform);
+        Texttest.AddComponent<CanvasRenderer>(); 
+        Text t = Texttest.AddComponent<Text>();
+        
+        t.text = "wee";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
